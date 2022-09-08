70ad5b892138529221aa01783e585b31c8d534ae
abdisoftware <abdisofware@gmail.com>
2022-09-08 00:06:55 -0300

    Creando Controladores y modificando el program para iniciar los entornos

---
+++ WepApi/Controllers/CharactersController.cs
@@ -0,0 +1,127 @@
+﻿using AutoMapper;
+using BusinessLogic.Data;
+using Core.DTO;
+using Core.Entities;
+using Core.Interfaces;
+using Core.Specifications;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WepApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CharactersController : ControllerBase
+    {
+        private readonly IGenericRepository<Personaje> _PersonGenericRepository;
+        private readonly IMapper _mapper;
+        private readonly IPeliculaRepository _peliculaRepository;
+        private readonly IPersonajeRepository _personajeRepository;
+
+        public CharactersController(IMapper mapper, IGenericRepository<Personaje> PersonGenericRepository, IPeliculaRepository peliculaRepository, IPersonajeRepository personajeRepository)
+        {
+            _PersonGenericRepository = PersonGenericRepository;
+            _mapper = mapper;
+            _peliculaRepository = peliculaRepository;
+            _personajeRepository = personajeRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<PersonajeDto>>> GetPersonaje(string? name, int? age, int? movie)
+        {
+            var spec = new PersonajeWithPersonajePeliculaAndSpesification(name, age, movie);
+            var Listperson = await _PersonGenericRepository.GetAllWithSpec(spec);
+
+            if (Listperson.Count == 0)
+            {
+                return NotFound();
+            }
+
+            var mRresponse = _mapper.Map< IReadOnlyList<Personaje>, IReadOnlyList<PersonajeDto>>(Listperson);
+
+            return Ok(mRresponse);
+        }
+
+      
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PersonajeDetalleDto>> GetPersonajeDetalle(int id)
+        {
+            var spec = new PersonajeWithPersonajePeliculaAndSpesification(id);
+            var person = await _PersonGenericRepository.GetByIdWithSpec(spec);
+
+            var mRresponse = _mapper.Map<Personaje, PersonajeDetalleDto>(person);
+
+
+            return Ok(mRresponse);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<PersonajeDto>> AddCharacter(PersonajeDataDto personaje)
+        {
+            //var listPelis = await _peliculaRepository.GetListPeliIdsAsinc(personaje);
+
+            //if (personaje.PeliculasId.Count != listPelis.Count)
+            //{
+            //    return BadRequest("No Existe Uno De Los Id de Peliculas Enviadas");
+            //}
+
+            var person = _mapper.Map<Personaje>(personaje);
+            var resp = await _PersonGenericRepository.CreateAsinc(person);
+            var spec = new PersonajeWithPersonajePeliculaAndSpesification(person.PersonajeId);
+            await _PersonGenericRepository.GetByIdWithSpec(spec);
+
+            if (resp == 0)
+            {
+                return BadRequest("No Se Pudo Crear la Pelicula");
+            }
+
+            var mpersonDto = _mapper.Map<Personaje, PersonajeDto>(person);
+
+            return Ok(mpersonDto);
+
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteCharacter(int id)
+        {
+            var person = await _PersonGenericRepository.GetByIdAsync(id);
+
+            if (person == null)
+            {
+                return NotFound("Pelicula No encontrada");
+            }
+
+            _PersonGenericRepository.DeleteAsinc(person);
+
+            return Content("Personaje Eliminado");
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> EditarPersonaje(int id, PersonajeDataDto personajeDataDto)
+        {
+            var spec = new PersonajeWithPersonajePeliculaAndSpesification(id);
+            var person = await _PersonGenericRepository.GetByIdWithSpec(spec);
+
+            if (person == null)
+            {
+                return NotFound("No existe el Personaje que desea Editar");
+            }
+
+            person = _mapper.Map(personajeDataDto, person);
+
+            var resp = await _PersonGenericRepository.UpdateAsinc(person);
+
+            if (resp == 0)
+            {
+                return NotFound("Eror al guardar la pelicula");
+            }
+
+            var upPerso = await _PersonGenericRepository.GetByIdWithSpec(spec);
+            var personDet = _mapper.Map<Personaje, PersonajeDetalleDto>(upPerso);
+
+            return Ok(personDet);
+        }
+
+    }
+}
---
+++ WepApi/Controllers/MoviesController.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Core.Interfaces;
+using Core.DTO;
+using Core.Entities;
+using Core.Specifications;
+using AutoMapper;
+using BusinessLogic.Data;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace WepApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IGenericRepository<Pelicula> _MovieRepository;
+        private readonly IPersonajeRepository _PersonRepository;
+        private readonly IPeliculaRepository _PeliRepository;
+        private readonly IMapper _mapper;
+
+        public MoviesController(IGenericRepository<Pelicula> MovieRepository, IMapper mapper, IGenericRepository<Pelicula> genericRepository, IPersonajeRepository PersonRepository, IPeliculaRepository PeliRepository)
+        {
+            _MovieRepository = MovieRepository;
+            _mapper = mapper;
+            _PersonRepository = PersonRepository;
+            _PeliRepository = PeliRepository;
+    }
+
+        [HttpGet]
+        public async Task<ActionResult<List<PeliculaDto>>> GetMovies(string? name,int? genre, string? order)
+        {
+            var spec = new PeliculaWithGeneroPersonajeSpecification(name, genre, order);
+            var movies = await _MovieRepository.GetAllWithSpec(spec);
+
+            var _movies =_mapper.Map<IReadOnlyList<Pelicula>, IReadOnlyList<PeliculaDto>>(movies);
+
+            return Ok(_movies);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PeliculaDetalleDto>> GetMoviesDetalle(int id)
+        {
+            var spec = new PeliculaWithGeneroPersonajeSpecification(id);
+            var movie = await _MovieRepository.GetByIdWithSpec(spec);
+
+            var _movies = _mapper.Map<Pelicula, PeliculaDetalleDto>(movie);
+
+            return Ok(_movies);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<PeliculaDetalleDto>> AddMovies(PeliculaDataDto pelicula)
+        {
+            var listPers = await _PersonRepository.GetListPersIdsAsinc(pelicula);
+
+            if (pelicula.PersonajeID.Count != listPers.Count)
+            {
+                return BadRequest("No Existe Uno De Los Personajes Enviados");
+            }
+
+            var movie = _mapper.Map<Pelicula>(pelicula);
+            var resp = await _MovieRepository.CreateAsinc(movie);
+            var spec = new PeliculaWithGeneroPersonajeSpecification(movie.PeliculaId);
+            await _MovieRepository.GetByIdWithSpec(spec);
+
+            if (resp == 0)
+            {
+                return BadRequest("No Se Pudo Crear la Pelicula");
+            }
+
+            var movieDto = _mapper.Map<Pelicula, PeliculaDetalleDto>(movie);
+
+            return Ok(movieDto);
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteMovies(int id)
+        {
+            var peli = await _MovieRepository.GetByIdAsync(id);
+
+            if (peli == null)
+            {
+                return NotFound("Pelicula No encontrada");
+            }
+            
+            _MovieRepository.DeleteAsinc(peli);
+
+            return Content("Pelicula Eliminada");
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> EditarPelicula(int id, PeliculaDataDto peliculaDataDto)
+        {
+            var spec = new PeliculaWithGeneroPersonajeSpecification(id);
+            var pelicula =  await _MovieRepository.GetByIdWithSpec(spec);
+
+            if (pelicula == null)
+            {
+                return NotFound("No existe la Pelicula que desea Editar");
+            }
+
+            pelicula = _mapper.Map(peliculaDataDto, pelicula);
+
+            var resp = await _MovieRepository.UpdateAsinc(pelicula);
+
+            if (resp == 0)
+            {
+                return NotFound("Eror al guardar la pelicula");
+            }
+
+            var upPeli = await _MovieRepository.GetByIdWithSpec(spec);
+            var movie = _mapper.Map<Pelicula, PeliculaDetalleDto>(upPeli);
+
+            return Ok(movie);
+
+        }
+    }
+
+}
