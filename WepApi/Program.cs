3041061b0779c08a80ae86ee73eb7dccfe027622
abdisoftware <abdisofware@gmail.com>
2022-09-08 00:06:12 -0300

    Configurando entorno instalando dependencias

--- WepApi/Program.cs
+++ WepApi/Program.cs
@@ -1,10 +1,48 @@
+using BusinessLogic.Data;
+using BusinessLogic.Logic;
+using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+using AutoMapper;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
-builder.Services.AddControllers();
+builder.Services.AddDbContext<ApiDisneyDbContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddAutoMapper(typeof(PeliculaRepository));
+builder.Services.AddTransient<IPeliculaRepository, PeliculaRepository>();
+builder.Services.AddTransient<IPersonajeRepository, PersonajeRepository>();
+
+
+builder.Services.AddControllers().AddJsonOptions(x =>
+   x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve)
+    .AddJsonOptions(x =>
+   x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var service = scope.ServiceProvider;
+    var loggerFactory = scope.ServiceProvider.GetService<ILoggerFactory>();
+
+    try
+    {
+        var context = service.GetRequiredService<ApiDisneyDbContext>();
+        context.Database.Migrate();
+    }
+    catch (Exception e)
+    {
+        var logger = loggerFactory.CreateLogger<Program>();
+        logger.LogError(e, "Error en el Proceso de Migración");
+    }
+}
 
 // Configure the HTTP request pipeline.
 
